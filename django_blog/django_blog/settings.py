bcee9a27c1fd4831aa325127d60a6003dd7061f9
Divine Darkey <teddbug47@outlook.com>
2021-11-15 01:16:53 -1200

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -149,6 +149,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'divinedarkey47@gmail.com'
-EMAIL_HOST_PASSWORD = 'stonebwoy'
+EMAIL_HOST_USER = '' # your email
+EMAIL_HOST_PASSWORD = '' # email password
 EMAIL_USE_TLS = True
